e62c8a8b0b657a308f9cb5b6d73b4f260d8b567e
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-05-23 12:58:50 +0600

    dev pulse server running

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import app from "./app";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
