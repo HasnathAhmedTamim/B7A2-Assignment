1b7accf2df140e27d33f1b0e705c4717c0a71cab
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-05-23 17:55:00 +0600

    update files

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,16 @@ import { IssueRoutes } from "./modules/issues/issue.route";
 
 const app = express();
 
-// middlewares
 app.use(cors());
 app.use(express.json());
 
-// health check route
 app.get("/", (req: Request, res: Response) => {
   res.send("DevPulse API is running");
 });
 
-// routes
 app.use("/api/auth", AuthRoutes);
 app.use("/api/issues", IssueRoutes);
 
-// not found and global error handler
 app.use(notFound);
 app.use(globalErrorHandler);
 
